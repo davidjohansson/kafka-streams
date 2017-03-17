480b0190a0007b75c06ae726f1d8ebbe01cb182b
davidjohansson <david.johansson@svt.se>
2017-03-17 15:33:15 +0100

    Adding some own stuff

--- src/main/java/bbejeck/stuff/MoistToPercent.java
+++ src/main/java/bbejeck/stuff/MoistToPercent.java
@@ -44,37 +44,37 @@ import java.util.Properties;
 public class MoistToPercent {
 
     public static void main(String[] args) throws Exception {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "moist-raw");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-
-        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
-        // Note: To re-run the demo, you need to use the offset reset tool:
-        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        KStreamBuilder builder = new KStreamBuilder();
-
-        KStream<String, String> source = builder.stream("moist-raw");
-
-        KTable<String, String> counts =
-                source
-                        .map((KeyValueMapper<String, String, KeyValue<K1, V1>>) (key, value) -> System.out.println())
-
-        // need to override value serde to Long type
-        counts.to(Serdes.String(), Serdes.Long(), "streams-wordcount-output");
-
-        KafkaStreams streams = new KafkaStreams(builder, props);
-        streams.cleanUp();
-
-        streams.start();
-
-        // usually the stream application would be running forever,
-        // in this example we just let it run for some time and stop since the input data is finite.
-//        Thread.sleep(50000L);
+//        Properties props = new Properties();
+//        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "moist-raw");
+//        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+//        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 //
-//        streams.close();
+//        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+//        // Note: To re-run the demo, you need to use the offset reset tool:
+//        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//
+//        KStreamBuilder builder = new KStreamBuilder();
+//
+//        KStream<String, String> source = builder.stream("moist-raw");
+//
+//        KTable<String, String> counts =
+//                source
+//                        .map((KeyValueMapper<String, String, KeyValue<K1, V1>>) (key, value) -> System.out.println());
+//
+//        // need to override value serde to Long type
+//        counts.to(Serdes.String(), Serdes.Long(), "streams-wordcount-output");
+//
+//        KafkaStreams streams = new KafkaStreams(builder, props);
+//        streams.cleanUp();
+//
+//        streams.start();
+//
+//        // usually the stream application would be running forever,
+//        // in this example we just let it run for some time and stop since the input data is finite.
+////        Thread.sleep(50000L);
+////
+////        streams.close();
     }
 }
