02eca964434492e0d9418ea21189fdb62f2a25d6
davidjohansson <david.u.johansson+github@gmail.com>
2017-10-04 22:08:28 +0200

    commit

---
+++ src/main/java/irrigation/MoistAverage.java
@@ -0,0 +1,46 @@
+package irrigation;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+import java.util.Properties;
+
+/**
+ * Demonstrates, using the high-level KStream DSL, how to implement the WordCount program
+ * that computes a simple word occurrence histogram from an input text.
+ *
+ * In this example, the input stream reads from a topic named "streams-file-input", where the values of messages
+ * represent lines of text; and the histogram output is written to topic "streams-wordcount-output" where each record
+ * is an updated count of a single word.
+ *
+ * Before running this example you must create the source topic (e.g. via bin/kafka-topics.sh --create ...)
+ * and write some data to it (e.g. via bin-kafka-console-producer.sh). Otherwise you won't see any data arriving in the output topic.
+ */
+public class MoistAverage {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "most-avg");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+
+/*        KStreamBuilder builder = new KStreamBuilder();
+
+
+        builder.stream("most-trans")
+
+                .groupByKey(Serdes.String(), Serdes.Integer())
+                .count(JoinWindows.of(1000))
+                .to(Serdes.String(), Serdes.Integer(), "most-trans-2");
+
+        KafkaStreams streams = new KafkaStreams(builder, props);
+        streams.start();*/
+    }
+}
